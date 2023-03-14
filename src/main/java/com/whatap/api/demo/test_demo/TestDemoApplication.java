a40f22385e25634db2eb3f34215a451f73488d0b
newkr <newkr@whatap.io>
2023-03-14 17:03:18 +0900

    failed deployed

--- src/main/java/com/whatap/api/demo/test_demo/TestDemoApplication.java
+++ src/main/java/com/whatap/api/demo/test_demo/TestDemoApplication.java
@@ -2,12 +2,18 @@ package com.whatap.api.demo.test_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestDemoApplication {
+public class TestDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestDemoApplication.class, args);
     }
-
 }
